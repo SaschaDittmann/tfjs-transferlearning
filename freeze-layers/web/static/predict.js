d40c043e4cc5c26859b8b3a96c5087706c8eadba
Sascha Dittmann <Sascha5812@gmx.de>
2020-11-01 00:41:04 +0100

    freeze layers example - web project added

---
+++ freeze-layers/web/static/predict.js
@@ -0,0 +1,60 @@
+let imageLoaded = false;
+$("#image-selector").change(function () {
+	imageLoaded = false;
+	let reader = new FileReader();
+	reader.onload = function () {
+		let dataURL = reader.result;
+		$("#selected-image").attr("src", dataURL);
+		$("#prediction-list").empty();
+		imageLoaded = true;
+	}
+	
+	let file = $("#image-selector").prop('files')[0];
+	reader.readAsDataURL(file);
+});
+
+function showProgress(percentage) {
+	var pct = Math.floor(percentage*100.0);
+	$('.progress-bar').html(`Loading Model (${pct}%)`);
+	console.log(`${pct}% loaded`);
+}
+
+let model;
+let modelLoaded = false;
+$( document ).ready(async function () {
+	$('.progress-bar').show();
+	console.log( "Loading model..." );
+	model = await tf.loadLayersModel('model/model.json', {onProgress: showProgress});
+    console.log( "Model loaded." );
+	$('.progress-bar').hide();
+	modelLoaded = true;
+});
+
+$("#predict-button").click(async function () {
+	if (!modelLoaded) { alert("The model must be loaded first"); return; }
+	if (!imageLoaded) { alert("Please select an image first"); return; }
+	let image = $('#selected-image').get(0);
+	
+	// Pre-process the image
+	let tensor = tf.browser.fromPixels(image)
+		.resizeNearestNeighbor([96,96])
+		.toFloat()
+		.div(tf.scalar(255.0))
+		.expandDims();
+
+	let predictions = await model.predict(tensor).data();
+	let top5 = Array.from(predictions)
+		.map(function (p, i) { // this is Array.map
+			return {
+				probability: p,
+				className: TARGET_CLASSES[i] // we are selecting the value from the obj
+			};
+		}).sort(function (a, b) {
+			return b.probability - a.probability;
+		}).slice(0, 5);
+
+	$("#prediction-list").empty();
+	top5.forEach(function (p) {
+		$("#prediction-list").append(`<li>${p.className}: ${p.probability.toFixed(6)}</li>`);
+		});
+});
